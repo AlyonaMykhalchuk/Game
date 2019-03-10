51deb2ec7926f764c00ac3f00b78ecd056a73b29
AlyonaMykhalchuk <alyonamyhalchuk@gmail.com>
2019-03-10 17:23:12 +0200

    half done

--- js.js
+++ js.js
@@ -1,22 +1,27 @@
+var gameSize = 3;
+var side = 100;
+var x1;
+var y1;
+var x2;
+var y2;
+var gameField = [gameSize];
+
 window.onload = function tictuc () {
     startGame();
 }
 
 
 function startGame() {
-    var gameSize = 3;
-    var side = 100;
-    var x1;
-    var y1;
-    var x2;
-    var y2;
-    var gameField = [gameSize][gameSize];
-    for (var i = 0; i < gameSize; i++) {
-        for (var j = 0; j < gameSize; j++) {
-            gameField[i][j] = "";
+    for (let i = 0; i < gameSize; i++) {
+        let line = [gameSize];
+        for (let j = 0; j < gameSize; j++) {
+            line[j] = "";
         }
+        gameField[i] = line;
     }
     var game = document.getElementById("game");
+
+    printGame();
 
     document.getElementById("clear").addEventListener("click", clearData);
 
@@ -26,19 +31,25 @@ function startGame() {
         console.log(event);
         var symbol = "0"
         if (event.target.className === "block") {
+            let id = event.target.getAttribute("id");
+            let array = id.split(',');
+            let i = array[0];
+            let j = array[1];
+
             if (step % 2 === 0) {
-                symbol = "X";
+                gameField[i][j] = "X";
             }
             else {
-                symbol = "0";
+                gameField[i][j] = "0";
             }
             event.target.innerText = symbol;
             event.target.className = "closed";
 
             step++;
-            if (step > 3) {
-                checkWinner();
-            }
+
+            checkWinner();
+
+            printGame();
         }
     }
 
@@ -64,19 +75,16 @@ function line(){
 
 function printGame() {
     var game = document.getElementById("game");
+    game.innerHTML = "";
     for (var i = 0; i < gameSize; i++) {
         for (var j = 0; j < gameSize; j++) {
             var htmlElem = document.createElement("div");
             htmlElem.setAttribute("class", "block");
             htmlElem.setAttribute("id", i + ',' + j);
-            htmlElem.setAttribute("onclick", )
-            htmlElem.innerText = gameField = [i][j];
-            game.innerHTML += htmlElem.value;
+            htmlElem.innerText = gameField[i][j];
+            console.log(htmlElem);
+            game.appendChild(htmlElem);
         }
     }
-}
-
-function changeElement(id) {
-
 }
 
